c9266f2ab33528fcbd0f03d64c4ac0b200251a30
mehdi-mtir <mehdi.mtir@gmail.com>
2022-10-25 16:35:59 +0200

    Crud complet sans sauvegarde

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const afficherLivres = ()=>{
                 <td>${livre.titre}</td>
                 <td>${livre.auteur}</td>
                 <td>${livre.prix}</td>
-                <td><button class='btn btn-primary'>Editer</button></td>
-                <td><button class='btn btn-danger' >Supprimer</button><td>
+                <td><button class='btn btn-primary' onclick='afficherFormulaireEdit(${livre.id})'>Editer</button></td>
+                <td><button class='btn btn-danger' onclick='supprimerLivre(${livre.id})' >Supprimer</button><td>
           </tr>`;
       }
     );
@@ -25,6 +25,17 @@ const afficherLivres = ()=>{
 
 const afficherFormulaireAjout = ()=>{
   document.getElementById("ajout").classList.remove("hide");
+}
+
+const afficherFormulaireEdit = (id)=>{
+  document.getElementById("edit").classList.remove("hide");
+  const livreAEditer = livres.find(
+    livre => livre.id == id
+  );
+  document.getElementById("titreEdit").value = livreAEditer.titre;
+  document.getElementById("auteurEdit").value = livreAEditer.auteur;
+  document.getElementById("prixEdit").value = livreAEditer.prix;
+  document.getElementById("idEdit").value = livreAEditer.id;
 }
 
 const ajoutLivre = (event)=>{
@@ -43,10 +54,42 @@ const ajoutLivre = (event)=>{
 
 }
 
+const editerLivre = (event)=>{
+  event.preventDefault();
+  const livreEdite = {
+    id : document.getElementById("idEdit").value,
+    titre : document.getElementById("titreEdit").value,
+    auteur : document.getElementById("auteurEdit").value,
+    prix : document.getElementById("prixEdit").value,
+  }
+  console.log(livreEdite);
+  livres = livres.map(
+    livre => {
+      if(livre.id == livreEdite.id)
+        return livreEdite
+      else
+        return livre
+    }
+  );
+  afficherLivres();
+  document.getElementById("edit").classList.add("hide");
+
+}
+
+const supprimerLivre = (id)=>{
+  if(confirm('Êtes-vous sûre de vouloir supprimer le livre?')){
+    livres = livres.filter(
+      livre => livre.id != id
+    );
+    afficherLivres();
+  }
+}
+
 
 const init = ()=>{
   afficherLivres();
   document.querySelector("#ajout form").addEventListener("submit", ajoutLivre);
+  document.querySelector("#edit form").addEventListener("submit", editerLivre);
   document.getElementById("btnAjout").addEventListener("click", afficherFormulaireAjout);
 }
 
